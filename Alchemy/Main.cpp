39c84d6b8a6f85d8a6493039ee03f82140552bd5
polblanescebrian <polblanescebrian@S1-44>
2017-04-18 13:58:44 +0200

    map

--- Alchemy/Main.cpp
+++ Alchemy/Main.cpp
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <unordered_map>
+#include <vector>
+#include <string>
+
 
 void main() {
+	std::unordered_map<std::string, std::pair<std::string, std::string>>mapa;
+
+	mapa["Dust"] = { "Air", "Earth" };
+	mapa["Gunpowder"] = { "Dust", "Fire" };
+
 
 
 }
