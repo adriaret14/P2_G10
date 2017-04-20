3a7b662b8ce175cc27c102142a8de6ad15baf547
polblanescebrian <polblanescebrian@S1-44>
2017-04-20 10:34:31 +0200

    Player header

--- Alchemy/Player.h
+++ Alchemy/Player.h
@@ -1,8 +1,27 @@
 #pragma once
+#include <vector>
+#include <string>
 class Player
 {
 public:
+
 	Player();
 	~Player();
+
+	void setScore( int );
+	int getScore();
+
+	void updateInv(std::string, std::string);
+	std::vector<std::string> getInv();
+
+	void updateDesc(std::string);
+	std::vector<std::string> getDesc();
+
+private:
+
+	int score;
+	std::vector<std::string>inventario;
+	std::vector<std::string>descubiertos;
+
 };
 
